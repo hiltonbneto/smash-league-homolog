4fe56c3df4be1613896c8f3d32fff47c8b5ab268
Hilton Neto <barroshilton56@gmail.com>
2025-04-10 00:22:28 -0300

    implementado funcionalidade retomar game

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4073336929"
+    serviceWorkerVersion: "3173419743"
   }
 });
