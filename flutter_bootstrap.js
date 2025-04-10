31f53b6af722f58cb3cf6e853e9b4377441910f7
Hilton Neto <barroshilton56@gmail.com>
2025-04-09 22:08:10 -0300

    Deploy Flutter Web para GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3164922851"
+    serviceWorkerVersion: "3487892375"
   }
 });
